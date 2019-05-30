50a6284c50937f36b9638750acd68139fe37e8b1
Luis Fernando <04fe1997@gamail.com>
2019-05-30 16:37:47 -0500

    es el inicio de nuestra pagina web

---
+++ partes/menu.php
@@ -0,0 +1,17 @@
+<nav class="contenedor">
+    <h1><h1>Patitas en Acción</h1></h1>
+   <nav class="subcontenedor">
+   <div>
+        <ul><a href="inicio.php">Inicio</a></ul>
+    </div>
+    <div>
+        <ul><a href="Reportes.php">Reportes</a></ul>
+    </div>
+    <div>
+        <ul><a href="Adopcion.php">Adopción</a></ul>
+    </div>
+    <div>
+        <ul><a href="esterilizacion.php">Programa de esterilización</a></ul>
+    </div>
+   </nav>
+</nav>
