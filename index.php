a1e6e91227a29ddca20c2b50a954cbd2154acdb8
Luis Fernando <04fe1997@gamail.com>
2019-06-04 16:41:00 -0500

    Añadido de mas estetica (cambio de fondo)

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>En acción</title>
+    <link rel="stylesheet" href="style/style.css">
+    <link rel="shortcut icon" href="imagen/logo.ico" type="image/x-icon">
+</head>
+<body>
+    <?php include 'partes/menu.php'; ?>
+    <div class="articulo">
+        <div>
+            <article>
+                Rescata o Adopta
+                <img src="http://www.chacohoy.com/files/imagenes/25072018191831.jpg">
+            </article> 
+        </div>
+        <div>
+             <article>
+                Somos una organizacion sin fines de lucro cuya finalidad es encontrar un hogar a mascotas
+                que han sufrido abandono o ayudar a aquellas personas que no encuentran espacio por número.
+            </article>
+        </div>
+    </div>
+</body>
+</html>
