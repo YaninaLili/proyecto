b63817cf3b2b57d9b63d01c35d606b8270b078b6
yanina lazaro <lilisapallanay@gmail.com>
2019-06-04 15:41:27 -0500

    formulario registro usuario

---
+++ registroUsuario.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <form action="procesarRegistro.php" method ="post">
+        <label >Nombre</label>
+        <input type="text" name="nombre">
+
+        <label >Apellido</label>
+        <input type="text" name="apellido">
+
+        <label >DNI</label>
+        <input type="text" name="dni">
+
+        <label >Direccion</label>
+        <input type="text" name="direccion">
+
+        <label >Correo Electronico</label>
+        <input type="text" name="direccion">
+
+        <label >Telefono</label>
+        <input type="text" name="telefono">
+
+        <label >Certificado de Salud Mental</label>
+        <input type="text" name="apellido">
+
+        <button>Registrar</button>
+
+    
+    </form>
+</body>
+</html>
