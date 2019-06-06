39a9ea186521425ecd62b03528bb5b1c2a357b64
Luis Fernando <04fe1997@gamail.com>
2019-06-06 09:56:32 -0500

    opcion de archivos cargados

--- cargar.php
+++ cargar.php
@@ -1,3 +1,4 @@
+<p>Suba el archivo de contancia psicologica</p>
 <form action="procesarCarga.php" method="post" enctype="multipart/form-data">
     <b>Enviar un nuevo archivo: </b>
     <input name="f" type="file">
