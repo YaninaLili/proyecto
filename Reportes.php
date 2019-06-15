836a9a2eedc657a9e6fdd519a777aedd93324456
Pilar <pilarfq261188@gmail.com>
2019-06-14 21:59:26 -0500

    ARREGLO DE FORMULARIO REPORTES

--- Reportes.php
+++ Reportes.php
@@ -8,63 +8,74 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link rel="stylesheet" href="style/styleReg.css">
 </head>
 <body>
-    <h1>Reportar mascotas abandonadas</h1>
-    <form action="formulario/procesarReportes.php" method = "post" enctype="multipart/form-data">
-        <h3>Datos de persona que encontró a la mascota:</h2>
-            <div>
-                <label>Nombres y Apellidos:</label>
-                <input type="text" name="n">
-            </div>
+<h1>Reportar mascotas abandonadas</h1>
+    <div class=padre>
+        <div class="hijo1">
+            <form action="formulario/procesarReportes.php" method = "post" enctype="multipart/form-data">
+                
+                <h3>Datos de persona que encontró a la mascota:</h3>
+                <div>
+                    <label>Nombres y Apellidos:</label>
+                    <input type="text" name="n">
+                </div>
 
-            <div>
-                <label>Teléfono:</label>
-                <input type="text" name="te">
-            </div>
+                <div>
+                    <label>Teléfono:</label>
+                    <input type="text" name="te">
+                </div>
 
-        <h3>Datos de mascota:</h3>
-            <div>
-                <label>Tipo:</label>
-                <select name="t1" id="tipo">
-                    <option value="S">Seleccionar</option>
-                    <option value="Perro">Perro</option>
-                    <option value="Gato">Gato</option>
-                    <option value="O">Otro</option>
-                </select>
-                <label>Especificar</label>
-                <input type="text" name="t2">
-            </div>
+                <h3>Datos de mascota:</h3>
+                <div>
+                    <label>Tipo:</label>
+                    <select name="t1" id="tipo">
+                        <option value="S">Seleccionar</option>
+                        <option value="Perro">Perro</option>
+                        <option value="Gato">Gato</option>
+                        <option value="Otro">Otro</option>
+                    </select>
+                    <label>Especificar:</label>
+                    <input type="text" name="t2">
+                </div>
 
-            <div>
-                <label>Sexo:</label>
-                <select name="s" id="sexo">
-                    <option value="E">Seleccionar</option>
-                    <option value="M">Macho</option>
-                    <option value="H">Hembra</option>
-                </select>
-            </div>
+                <div>
+                    <label>Sexo:</label>
+                    <select name="s" id="sexo">
+                        <option value="E">Seleccionar</option>
+                        <option value="M">Macho</option>
+                        <option value="H">Hembra</option>
+                    </select>
+                </div>
 
-            <div>
-                <label>Tamaño:</label>
-                <input type="text" name="ta">
-            </div>
+                <div>
+                    <label>Tamaño:</label>
+                    <input type="text" name="ta">
+                </div>
 
-            <div>
-                <label>Lugar donde fue encontrada:</label>
-                <input type="text" name="l">
-            </div>
+                <div>
+                    <label>Lugar donde fue encontrada:</label>
+                    <input type="text" name="l">
+                </div>
 
-            <div>
-                <input type="file" name="fotoMascota">
-            </div>
+                <div>
+                    <label>Foto:</label>
+                    <input id="foto" type="file" name="fotoMascota" >
+                </div>
 
-            <div>
-                <label>Descripción de estado:</label><br>
-                <textarea name="d" id="" cols="30" rows="5"></textarea>
-            </div>
-            <button>Guardar</button>
-    </form>
-    
+                <div>
+                    <label>Descripción de estado:</label>
+                    <textarea name="d" id="" cols="30" rows="5"></textarea>
+                </div>
+                <button>Guardar</button>              
+            </form>
+        </div>
+
+        <div class="hijo2">
+            <img src="imagen/perrito.jpg"><br>
+            <img src="imagen/perro.jpg">
+        </div>
+    </div>
 </body>
 </html>
--- formulario/registroNuevo.php
+++ formulario/registroNuevo.php
@@ -12,72 +12,69 @@
     <div class="padre1">
 
         <div class="im" >
-        <img src="https://www.abogacia.es/wp-content/uploads/2017/05/Tips-que-hacer-con-tu-mascota-cuando-sales-de-viaje-1500x1000.jpg" width="600" height="600" >
+            <img src="https://www.abogacia.es/wp-content/uploads/2017/05/Tips-que-hacer-con-tu-mascota-cuando-sales-de-viaje-1500x1000.jpg" width="600" height="600" >
         </div>
         
         <div class="hijo1">
-        <h1>Crear una cuenta</h1>
-        <form action="formulario/procesarRegistro.php" method ="post">
+            <h1>Crear una cuenta</h1>
+            <form action="formulario/procesarRegistro.php" method ="post">
     
-        <div>
-        <label >Nombre</label>
-        <input type="text" name="n" >
-        </div>
-        <div>     <label >Apellido</label>
-        <input type="text" name="a" >
-        </div>
+            <div>
+                <label >Nombre</label>
+                <input type="text" name="n" >
+            </div>
+            
+            <div>     
+                <label >Apellido</label>
+                <input type="text" name="a" >
+            </div>
 
-        <div>
-            <label >Nombre de Usuario</label>
-            <input type="text" name="u" >
-        </div>
+            <div>
+                <label >Nombre de Usuario</label>
+                <input type="text" name="u" >
+            </div>
         
-        <div>
-        <label >Correo Electronico</label>
-        <input type="email" name="e" >
-        </div>
+            <div>
+                <label >Correo Electronico</label>
+                <input type="email" name="e" >
+            </div>
 
-        <div>
-        <label >sexo</label>
-        <select name="s" id="tipoSexo">
-            <option >Seleccionar</option>
-            <option value="f">Femenino </option>
-            <option value="m">Masculino </option>
-        </select>
-        </div>
+            <div>
+                <label >sexo</label>
+                <select name="s" id="tipoSexo">
+                    <option >Seleccionar</option>
+                    <option value="f">Femenino </option>
+                    <option value="m">Masculino </option>
+                </select>
+            </div>
         
-        <div>
-        <label >DNI</label>
-        <input type="text" name="d">
-        </div>
+            <div>
+                <label >DNI</label>
+                <input type="text" name="d">
+            </div>
         
-        <div>
-        <label >Direccion</label>
-        <input type="text" name="dir">
-        </div>
+            <div>
+                <label >Direccion</label>
+                <input type="text" name="dir">
+            </div>
 
-        
+            <div>
+                <label >Teléfono</label>
+                <input type="text" name="t">
+            </div>
 
-        <div>
-        <label >Telefono</label>
-        <input type="text" name="t">
-        </div>
+            <div>
+                <label >Contraseña</label>
+                <input type="password" name="c">
+            </div>
 
-        <div>
-        <label >Contraseña</label>
-        <input type="password" name="c">
-        </div>
-
-        <div>
-        <label >Confirmar Contraseña</label>
-        <input type="password" name="c2" >
-        </div>
-        <button>Registrarte</button>
-
-    
+            <div>
+                <label >Confirmar Contraseña</label>
+                <input type="password" name="c2" >
+            </div>
+            <button>Registrarte</button>
         </form>
         </div>
     </div>
-
 </body>
 </html>
